a79ef74df9c6db67a29c9f3377b99a71c06d5bdf
Yokey <zyf6@outlook.com>
2021-04-04 21:53:37 +0800

    Initial Commit

---
+++ miniprogram/pages/answer/answer.js
@@ -0,0 +1,120 @@
+// pages/databaseGuide/databaseGuide.js
+
+const app = getApp()
+
+Page({
+
+  data: {
+    step: 1,
+    counterId: '',
+    openid: '',
+    count: null,
+    queryResult: '',
+    nickName: '',
+
+    formData: {},
+    quiz: {
+      text: "这是一道题目的题干",
+      ans: 0,
+      ismulti: false,
+      items: [{
+          name: '这是选项 0 选我就对了',
+          value: '0'
+        },
+        {
+          name: '这是选项 1 选我就错了',
+          value: '1'
+        }
+      ],
+    },
+
+    questions: [{
+      text: "这是一道题目的题干",
+      ans: 0,
+      ismulti: false,
+      items: [{
+          name: '这是选项 0 选我就对了',
+          value: '0'
+        },
+        {
+          name: '这是选项 1 选我就错了',
+          value: '1'
+        }
+      ],
+    }],
+  },
+
+
+
+  onLoad: function (options) {
+    if (app.globalData.openid) {
+      this.setData({
+        openid: app.globalData.openid,
+        nickName: app.globalData.nickName
+      })
+    }
+  },
+
+  getQuiz() {
+    db.collection('quiz').aggregate().sample({
+        size: 1
+      }).end()
+      .then(res => {
+        console.log(res.list[0]);
+        this.setData({
+          quiz: res.list[0]
+        })
+      })
+  },
+
+
+  submitForm() {
+    console.log(this.data.formData)
+  },
+
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.quiz.items;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems,
+      [`formData.radio`]: e.detail.value
+    });
+  },
+
+
+  nextStep: function () {
+    // 在第一步，需检查是否有 openid，如无需获取
+    
+      this.setData({
+        step: this.data.step + 1
+      }, callback)
+    
+  },
+
+  prevStep: function () {
+    this.setData({
+      step: this.data.step - 1
+    })
+  },
+
+  goHome: function () {
+    const pages = getCurrentPages()
+    if (pages.length === 2) {
+      wx.navigateBack()
+    } else if (pages.length === 1) {
+      wx.redirectTo({
+        url: '../index/index',
+      })
+    } else {
+      wx.reLaunch({
+        url: '../index/index',
+      })
+    }
+  }
+
+})
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,74 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    takeSession: false,
+    requestResult: '',
+  },
+
+  onLoad: function() {
+      // 调用云函数
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log('[云函数] [login] user openid: ', res.result.openid)
+          app.globalData.openid = res.result.openid
+        },
+        fail: err => {
+          console.error('[云函数] [login] 调用失败', err)
+        }
+      })
+
+      wx.getSetting({
+        success(res) {
+          if (!res.authSetting['scope.werun']) {
+            wx.authorize({
+              scope: 'scope.werun',
+            })
+          }
+        }
+      })
+
+  },
+
+  getUserProfile() {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          avatarUrl: res.userInfo.avatarUrl,
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        })
+        app.globalData.avatarUrl=res.userInfo.avatarUrl
+        app.globalData.nickName=res.userInfo.nickName
+        app.globalData.hasUserInfo=true
+      }
+      
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true,
+      })
+    }
+  },
+
+  onScanQRCode() {
+    wx.scanCode({
+      onlyFromCamera: true,
+      success (res) {
+        console.log(res)
+      }
+    })
+  }
+
+})
